fbe886a72768038faad9a2cd55add5e080282b2e
genie-ru <genie.zgmf987@gmail.com>
2023-05-21 07:33:50 +0900

    学習用

--- 01_js_basic/020_arrow_function/start/main.js
+++ 01_js_basic/020_arrow_function/start/main.js
@@ -2,7 +2,24 @@ function fn(number) {
   return number * 2;
 }
 
-
+// Arrow function
 const fnArrow = (number) => {
   return number * 2;
 }
+
+// Arrow function with one parameter
+
+const fnArrowOneParam = number => {
+  return number * 2;
+}
+// =>の後には{}を入力すると,重複するためもし{}を入力したい場合は()で囲む必要がある。
+// 下の文ではresultにnumber*2を代入している。とうオブジェクトになる。
+const fnArrowOneParam1 = (number) => ({ result: number * 2 })
+console.log(fnArrowOneParam1(2));
+
+//But if you have only one line of code in the function body, you can omit the curly braces and the return statement  
+const fnArrowOneParamOneLine = number => number * 2;
+
+console.log(fn(2));
+
+
