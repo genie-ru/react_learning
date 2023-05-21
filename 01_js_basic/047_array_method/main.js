cbadfa89c299850006b71ef7cb8120d2df8f0419
genie-ru <genie.zgmf987@gmail.com>
2023-05-21 11:13:02 +0900

    学習用

--- 01_js_basic/047_array_method/main.js
+++ 01_js_basic/047_array_method/main.js
@@ -2,10 +2,19 @@ const arry = [10, 20, 30, 40];
 const newarry = [];
 
 for (let i = 0; i < arry.length; i++) {
-    newarry.push(arry[i] * 2);
+    const val = arry[i] * 2;
+    if(val > 50) {
+        newarry.push(arry[i] * 2);
+    }
 }   
 
 console.log(newarry);
 
-const newarry2 = arry.map(val => val * 2);
-console.log(newarry2);
+/*
+const newArry2 = arry.map(val =>  val * 2);
+const newArry3 = newArry2.filter(val => val > 50);
+console.log(newArry3);
+*/
+const newArry2 = arry.map(val =>  val * 2).filter(val => val > 50);
+console.log(newArry2);
+
