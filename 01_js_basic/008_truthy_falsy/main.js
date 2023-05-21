d6dfb1cd29b631d30721f1183dde3594ea2afba4
genie-ru <genie.zgmf987@gmail.com>
2023-05-22 00:33:13 +0900

    学習用

---
+++ 01_js_basic/008_truthy_falsy/main.js
@@ -0,0 +1,16 @@
+const a = true;
+let resultA;
+
+if(a) {
+    resultA = "true";
+} else {
+    resultA = "false";
+}
+
+console.log(resultA);
+
+function getResult() {
+    return a ? "true" : "false";
+}
+
+console.log(getResult());
