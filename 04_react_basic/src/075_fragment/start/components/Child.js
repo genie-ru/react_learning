3093aea33b5540c4cb8402e86d04a5153e6fd9cb
genie-ru <genie.zgmf987@gmail.com>
2023-05-23 04:29:22 +0900

    test

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -2,9 +2,15 @@ import "./Child.css";
 
 const Child = () => {
   return (
+    //key属性のみFragmentに指定できる
+    <>
     <div className="component">
       <h3>Hello Component</h3>
     </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem</p>
+    
+    </>
   );
 };
 
