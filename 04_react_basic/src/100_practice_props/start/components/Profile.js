7f444924c3bf220800b9c666fe43c60fae83a702
genie-ru <genie.zgmf987@gmail.com>
2023-05-24 18:36:29 +0900

    学習用

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,9 +1,11 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({name = "John Doe", age = "??", country = "Japan"}) => {
   return (
     <div className="profile">
-
+      <h2>Name: {name}</h2>
+      <h3>Age: {age}</h3>
+      <h3>From: {country}</h3>
     </div>
   );
 };
